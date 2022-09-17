d8fc77beabce7e5cec9e1ec40d7f2d424192e787
Acer <Acer@DESKTOP-CERFMEQ>
2022-09-17 18:43:18 +0800

    Brick Breaker Game

--- src/Brickdemolisher/Main.java
+++ src/Brickdemolisher/Main.java
@@ -3,6 +3,13 @@ import javax.swing.JFrame;
 public class Main {
     public static void main(String[] args) {
         JFrame obj = new JFrame();
+        Gameplay gamePlay = new Gameplay();
+        obj.setBounds(10, 10, 700, 600);
+        obj.setTitle("Brick Demolisher");
+        obj.setResizable(false);
+        obj.setVisible(true);
+        obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        obj.add(gamePlay);
     }
     
 }
